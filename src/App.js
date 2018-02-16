ad839888fdf7c2262cc14fc7aff15afabe1a5612
ilebedev <ilebedev@playflock.com>
2018-02-16 15:29:57 +0300

    next step

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import cn from 'classnames';
 import _ from 'lodash';
 import './App.css';
 import SidePanel from './SidePanel';
 import SideMenu from './SideMenu';
 import Notifications from './Notifications';
-import Filters from './Notifications';
+import Filters from './Filters';
+
+const COLUMNS = [
+    'Идея',
+    'План',
+    'В работе',
+    'Приём',
+    'Завершено',
+];
 
 export default class App extends Component {
 
     constructor(props) {
         super(props);
 
-        this._column     = 0;
         this._shift      = 0;
         this._velocity   = 0;
         this._lastMoveTs = 0;
         this._startTouch = null;
         this._prevTouch  = null;
+
+        this.state = {
+            column:  0,
+            inSwipe: false,
+        };
 
         _.bindAll(this, [
             '_onTouchStart',
@@ -33,22 +46,30 @@ export default class App extends Component {
     }
 
     render() {
+        const { column, inSwipe } = this.state;
+
         return (
             <div className="app">
                 <div className="header">
-                    <div className="header-part">
-                        <div className="button" onClick={this._onSideMenuClick}>M</div>
-                        <div className="button" onClick={this._onFiltersClick}>F</div>
+                    <div className={cn('header__column-name', {
+                        'header__column-name_in-swipe': inSwipe,
+                    })}>
+                        {COLUMNS[column]}
                     </div>
-                    <div className="header-part">
-                        <div className="button"></div>
-                        <div className="button" onClick={this._onNotificationsClick}>N</div>
+                    <div className="header__buttons">
+                        <div className="header-part">
+                            <div className="button" onClick={this._onSideMenuClick}>M</div>
+                            <div className="button" onClick={this._onFiltersClick}>F</div>
+                        </div>
+                        <div className="header-part">
+                            <div className="button"></div>
+                            <div className="button" onClick={this._onNotificationsClick}>N</div>
+                        </div>
                     </div>
                 </div>
                 <div className="board-wrapper" ref="boardWrapper">
                     <div className="board" ref="board">
                         <div className="column">
-                            <div className="column-header">Идея</div>
                             <div className="task"></div>
                             <a href="//yandex.ru">
                                 <div className="task"></div>
@@ -62,23 +83,19 @@ export default class App extends Component {
                             <div className="task"></div>
                         </div>
                         <div className="column">
-                            <div className="column-header">План</div>
                             <div className="task"></div>
                             <div className="task"></div>
                         </div>
                         <div className="column">
-                            <div className="column-header">В работе</div>
                             <div className="task"></div>
                             <div className="task"></div>
                             <div className="task"></div>
                         </div>
                         <div className="column">
-                            <div className="column-header">Приёмка</div>
                             <div className="task"></div>
                             <div className="task"></div>
                         </div>
                         <div className="column">
-                            <div className="column-header">Завершено</div>
                             <div className="task"></div>
                             <div className="task"></div>
                             <div className="task"></div>
@@ -98,15 +115,23 @@ export default class App extends Component {
         );
     }
 
-    _clear() {
-        this._startTouch = null;
-        this._prevTouch  = null;
-        this._lastMoveTs = 0;
-        this._velocity   = 0;
+    _clear(notFull) {
+        if (!notFull) {
+            this._startTouch = null;
+            this._prevTouch  = null;
+            this._lastMoveTs = 0;
+            this._velocity   = 0;
+        }
 
         cancelAnimationFrame(this._anim);
         document.removeEventListener('touchmove', this._onTouchMove);
         document.removeEventListener('touchend', this._onTouchEnd);
+
+        if (this.state.inSwipe) {
+            this.setState({
+                inSwipe: false,
+            });
+        }
     }
 
     _onTouchStart(e) {
@@ -153,6 +178,10 @@ export default class App extends Component {
             this._prevTouch  = touch;
             this._lastMoveTs = Date.now();
             e.preventDefault();
+
+            this.setState({
+                inSwipe: true,
+            });
             return;
         }
 
@@ -160,7 +189,7 @@ export default class App extends Component {
 
         this._velocity = Math.abs((this._prevTouch.x - touch.x) / (Date.now() - this._lastMoveTs));
 
-        this._setShift(-this._column * 100 + (touch.x - this._startTouch.x) / this._width);
+        this._setShift(-this.state.column * 100 + (touch.x - this._startTouch.x) / this._width);
 
         this._lastMoveTs = Date.now();
         this._prevTouch = touch;
@@ -173,34 +202,38 @@ export default class App extends Component {
 
         e.preventDefault();
 
+        this._clear(true);
+
         const startShift = this._shift;
 
         const n = -this._shift / 100;
 
+        let column;
+
         if (n < 0) {
-            this._column = 0;
+            column = 0;
         } else if (n > 4) {
-            this._column = 4;
+            column = 4;
         } else {
             const min = Math.floor(n);
             const max = Math.ceil(n);
 
             if (this._velocity > 0.5) {
                 if (this._prevTouch.x < this._startTouch.x) {
-                    this._column = max;
+                    column = max;
                 } else {
-                    this._column = min;
+                    column = min;
                 }
             } else {
                 if (-this._shift - min * 100 < 50) {
-                    this._column = min;
+                    column = min;
                 } else {
-                    this._column = max;
+                    column = max;
                 }
             }
         }
 
-        const goingTo = -this._column * 100;
+        const goingTo = -column * 100;
 
         const steps = Math.abs(this._shift - goingTo);
 
@@ -224,6 +257,11 @@ export default class App extends Component {
         };
 
         this._anim = requestAnimationFrame(tick);
+
+        this.setState({
+            column,
+            inSwipe: false,
+        });
     }
 
     _setShift(x) {

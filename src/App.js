d63bae4feeeb31e44ed7406c08d608e4e2f61c1b
ilebedev <ilebedev@playflock.com>
2018-02-16 12:55:27 +0300

    step 1

---
+++ src/App.js
@@ -0,0 +1,219 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import './App.css';
+import Notifications from './Notifications';
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this._column     = 0;
+        this._shift      = 0;
+        this._velocity   = 0;
+        this._lastMoveTs = 0;
+        this._startTouch = null;
+        this._prevTouch  = null;
+
+        _.bindAll(this, [
+            '_onTouchStart',
+            '_onTouchEnd',
+            '_onTouchMove',
+            '_onNotificationsClick',
+        ]);
+    }
+
+    componentDidMount() {
+        document.addEventListener('touchstart', this._onTouchStart);
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div className="header">
+                    <div className="header-part">
+                        <div className="button"></div>
+                        <div className="button"></div>
+                    </div>
+                    <div className="header-part">
+                        <div className="button"></div>
+                        <div className="button" onClick={this._onNotificationsClick}></div>
+                    </div>
+                </div>
+                <div className="board-wrapper" ref="boardWrapper">
+                    <div className="board" ref="board">
+                        <div className="column">
+                            <div className="column-header">Идея</div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                        </div>
+                        <div className="column">
+                            <div className="column-header">План</div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                        </div>
+                        <div className="column">
+                            <div className="column-header">В работе</div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                        </div>
+                        <div className="column">
+                            <div className="column-header">Приёмка</div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                        </div>
+                        <div className="column">
+                            <div className="column-header">Завершено</div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                            <div className="task"></div>
+                        </div>
+                    </div>
+                </div>
+                <Notifications ref="notifications" />
+            </div>
+        );
+    }
+
+    _clear() {
+        this._startTouch = null;
+        this._prevTouch  = null;
+        this._lastMoveTs = 0;
+        this._velocity   = 0;
+
+        cancelAnimationFrame(this._anim);
+        document.removeEventListener('touchmove', this._onTouchMove);
+        document.removeEventListener('touchend', this._onTouchEnd);
+    }
+
+    _onTouchStart(e) {
+        this._clear();
+
+        if (this.refs.boardWrapper.contains(e.target) && e.touches.length === 1) {
+            this._initialTouch = {
+                x: e.touches[0].clientX,
+                y: e.touches[0].clientY,
+            };
+
+            this._firstMove = true;
+
+            this._width = this.refs.board.clientWidth / 100;
+
+            document.addEventListener('touchmove', this._onTouchMove, {
+                passive: false,
+            });
+            document.addEventListener('touchend', this._onTouchEnd, {
+                passive: false,
+            });
+
+            //e.preventDefault();
+        }
+    }
+
+    _onTouchMove(e) {
+        const touch = {
+            x: e.touches[0].clientX,
+            y: e.touches[0].clientY,
+        };
+
+        if (this._firstMove) {
+            this._firstMove = false;
+            const deltaX = Math.abs(this._initialTouch.x - touch.x);
+            const deltaY = Math.abs(this._initialTouch.y - touch.y);
+
+            if (deltaX < deltaY) {
+                this._clear();
+                return;
+            }
+
+            this._startTouch = touch;
+            this._prevTouch  = touch;
+            this._lastMoveTs = Date.now();
+            e.preventDefault();
+            return;
+        }
+
+        e.preventDefault();
+
+        this._velocity = Math.abs((this._prevTouch.x - touch.x) / (Date.now() - this._lastMoveTs));
+
+        this._setShift(-this._column * 100 + (touch.x - this._startTouch.x) / this._width);
+
+        this._lastMoveTs = Date.now();
+        this._prevTouch = touch;
+    }
+
+    _onTouchEnd(e) {
+        e.preventDefault();
+
+        const startShift = this._shift;
+
+        const n = -this._shift / 100;
+
+        if (n < 0) {
+            this._column = 0;
+        } else if (n > 4) {
+            this._column = 4;
+        } else {
+            const min = Math.floor(n);
+            const max = Math.ceil(n);
+
+            if (this._velocity > 0.5) {
+                if (this._prevTouch.x < this._startTouch.x) {
+                    this._column = max;
+                } else {
+                    this._column = min;
+                }
+            } else {
+                if (-this._shift - min * 100 < 50) {
+                    this._column = min;
+                } else {
+                    this._column = max;
+                }
+            }
+        }
+
+        const goingTo = -this._column * 100;
+
+        const steps = Math.abs(this._shift - goingTo);
+
+        let start  = null;
+        let prevTs = null;
+
+        const tick = ts => {
+            if (!prevTs) {
+                start  = ts;
+                prevTs = ts;
+                this._anim = requestAnimationFrame(tick);
+                return;
+            }
+
+            const ease = 1 - Math.min(1, (ts - start) / (steps * 3));
+            this._setShift(goingTo + (startShift - goingTo) * ease);
+
+            if (ease > 0) {
+                this._anim = requestAnimationFrame(tick);
+            }
+        };
+
+        this._anim = requestAnimationFrame(tick);
+    }
+
+    _setShift(x) {
+        this._shift = x;
+        this.refs.board.style.transform = `translateX(${x}%)`;
+    }
+
+    _onNotificationsClick() {
+        this.refs.notifications.show();
+    }
+
+}
---
+++ src/Notifications.js
@@ -0,0 +1,210 @@
+import React, { Component } from 'react';
+import cn from 'classnames';
+import _ from 'lodash';
+import './Notifications.css';
+
+export default class Notifications extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show:   false,
+            rising: false,
+            manual: false,
+            hiding: false,
+        };
+
+        _.bindAll(this, [
+            '_onBgClick',
+            '_onTouchStart',
+            '_onTouchEnd',
+            '_onTouchMove',
+        ]);
+    }
+
+    componentDidMount() {
+        document.addEventListener('touchstart', this._onTouchStart);
+    }
+
+    render() {
+        const { show, rising, manual, hiding } = this.state;
+
+        if (!show) {
+            return null;
+        }
+
+        return (
+            <div className={cn('notifications', {
+                'notifications_rising':    rising,
+                'notifications_hiding':    hiding,
+                'notifications_move-back': hiding && !manual,
+            })} ref="root" onTouchStart={this._onBgClick}>
+                <div className="notification-panel" ref="panel">
+                    <div className="notification"></div>
+                    <div className="notification"></div>
+                    <div className="notification"></div>
+                    <div className="notification"></div>
+                </div>
+            </div>
+        );
+    }
+
+    show() {
+        this._shift = 0;
+
+        this.setState({
+            show:   true,
+            rising: true,
+            manual: false,
+        });
+
+        setTimeout(() => {
+            this.setState({
+                rising: false,
+            });
+        }, 300);
+    }
+
+    _onBgClick(e) {
+        if (e.target === this.refs.root && !this.state.hiding) {
+            this._close();
+        }
+    }
+
+    _clear() {
+        this._startTouch = null;
+        this._prevTouch  = null;
+        this._lastMoveTs = 0;
+        this._velocity   = 0;
+
+        cancelAnimationFrame(this._anim);
+        document.removeEventListener('touchmove', this._onTouchMove);
+        document.removeEventListener('touchend', this._onTouchEnd);
+    }
+
+    _onTouchStart(e) {
+        if (!this.state.show) {
+            return;
+        }
+
+        this._clear();
+
+        if (this.refs.panel.contains(e.target) && e.touches.length === 1) {
+            this._initialTouch = {
+                x: e.touches[0].clientX,
+                y: e.touches[0].clientY,
+            };
+
+            this._firstMove = true;
+
+            this._width = this.refs.panel.clientWidth / 100;
+
+            document.addEventListener('touchmove', this._onTouchMove, {
+                passive: false,
+            });
+            document.addEventListener('touchend', this._onTouchEnd, {
+                passive: false,
+            });
+        }
+    }
+
+    _onTouchMove(e) {
+        const touch = {
+            x: e.touches[0].clientX,
+            y: e.touches[0].clientY,
+        };
+
+        if (this._firstMove) {
+            this._firstMove = false;
+            const deltaX = Math.abs(this._initialTouch.x - touch.x);
+            const deltaY = Math.abs(this._initialTouch.y - touch.y);
+
+            if (deltaX < deltaY) {
+                this._clear();
+                return;
+            }
+
+            this._startTouch = touch;
+            this._prevTouch  = touch;
+            this._lastMoveTs = Date.now();
+            e.preventDefault();
+            return;
+        }
+
+        e.preventDefault();
+
+        this._velocity = Math.abs((this._prevTouch.x - touch.x) / (Date.now() - this._lastMoveTs));
+
+        this._setShift((touch.x - this._startTouch.x) / this._width);
+
+        this._lastMoveTs = Date.now();
+        this._prevTouch = touch;
+    }
+
+    _onTouchEnd(e) {
+        e.preventDefault();
+
+        const startShift = this._shift;
+
+        let close = false;
+
+        if (this._velocity > 0.5) {
+            close = this._prevTouch.x > this._startTouch.x;
+        } else {
+            close = this._shift > 20;
+        }
+
+        const goingTo = close ? 100 : 0;
+
+        const ratio = 180 * (Math.abs(this._shift - goingTo) / 100);
+
+        let start  = null;
+        let prevTs = null;
+
+        const tick = ts => {
+            if (!prevTs) {
+                start  = ts;
+                prevTs = ts;
+                this._anim = requestAnimationFrame(tick);
+                return;
+            }
+
+            const ease = 1 - Math.min(1, (ts - start) / ratio);
+            this._setShift(goingTo + (startShift - goingTo) * ease);
+
+            if (ease > 0) {
+                this._anim = requestAnimationFrame(tick);
+            } else {
+                this._clear();
+            }
+        };
+
+        this._anim = requestAnimationFrame(tick);
+
+        if (close) {
+            this._close(true);
+        }
+    }
+
+    _setShift(x) {
+        this._shift = Math.max(0, x);
+        this.refs.panel.style.transform = `translateX(${this._shift}%)`;
+    }
+
+    _close(isSwipe) {
+        this.setState({
+            manual: isSwipe,
+            hiding: true,
+        });
+
+        setTimeout(() => {
+            this.setState({
+                manual: false,
+                hiding: false,
+                show:   false,
+            });
+        }, 200);
+    }
+
+}
